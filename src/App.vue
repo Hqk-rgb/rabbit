dd99baaee2e70bb385552c7286115ded8235a5df
OP <1332869575@qq.com>
2023-06-30 15:44:28 +0800

    feat(custom): scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 </script>
 
 <template>
-  <div>
-    <el-row class="mb-4">
-      <el-button type="primary">Primary</el-button>
-    </el-row>
-    <!-- 一级路由出口组件 -->
-    <RouterView />
-  </div>
+	<div>
+		<el-row class="mb-4">
+			<el-button type="primary">Primary</el-button>
+			<div class="test">test css</div>
+		</el-row>
+		<!-- 一级路由出口组件 -->
+		<RouterView />
+	</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test {
+	color: $priceColor;
+}
 </style>
