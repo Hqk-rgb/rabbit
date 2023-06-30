e8d9fb3f7f9ed9ff47d7200d536cacba70bcfee1
OP <1332869575@qq.com>
2023-06-30 16:07:46 +0800

    feat(custom): 字体图标引入

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@
 
 <template>
 	<div>
-		<el-row class="mb-4">
-			<el-button type="primary">Primary</el-button>
-			<div class="test">test css</div>
-		</el-row>
 		<!-- 一级路由出口组件 -->
 		<RouterView />
 	</div>
