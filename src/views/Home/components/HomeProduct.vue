2494835f852a4a1e9624b62031633f6468329d7a
OP <1332869575@qq.com>
2023-07-10 14:09:42 +0800

    feat(custom): 导航栏路由跳转配置

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -23,7 +23,7 @@ onMounted(() => getGoods())
 					</strong>
 				</RouterLink>
 				<ul class="goods-list">
-					<li v-for="good in cate.goods" :key="good.id">
+					<li v-for="goods in cate.goods" :key="goods.id">
 						<GoodsItem :goods="goods" />
 					</li>
 				</ul>
