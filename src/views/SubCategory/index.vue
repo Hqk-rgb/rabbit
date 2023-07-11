2fec7a9173e87617a5734dafcb6949e66c7fb1ee
OP <1332869575@qq.com>
2023-07-11 11:48:53 +0800

    feat(custom): 二级分类面包屑导航

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,4 +1,19 @@
 <script setup>
+import { getCategoryFilterAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+//获取面包屑导航数据
+const route = useRoute()
+const categoryData = ref({})
+const getCategoryData = async () => {
+	getCategoryFilterAPI()
+	const res = await getCategoryFilterAPI(route.params.id)
+	categoryData.value = res.result
+}
+onMounted(() => {
+	getCategoryData()
+})
 </script>
 
 <template>
@@ -7,8 +22,8 @@
 		<div class="bread-container">
 			<el-breadcrumb separator=">">
 				<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-				<el-breadcrumb-item :to="{ path: '/' }">居家 </el-breadcrumb-item>
-				<el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+				<el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }} </el-breadcrumb-item>
+				<el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
 			</el-breadcrumb>
 		</div>
 		<div class="sub-container">
