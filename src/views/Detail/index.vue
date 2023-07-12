b9a7747ae46a4a790b188bbdfbc144cb3c3dbcb9
OP <1332869575@qq.com>
2023-07-12 14:41:43 +0800

    feat(custom): 详情页基础组件封装（热榜）

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { getDetail } from '@/apis/detail'
+import DetailHot from '@/views/Detail/components/DetailHot.vue'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
@@ -111,7 +112,10 @@ onMounted(() => {
 							</div>
 						</div>
 						<!-- 24热榜+专题推荐 -->
-						<div class="goods-aside"></div>
+						<div class="goods-aside">
+							<DetailHot></DetailHot>
+							<DetailHot />
+						</div>
 					</div>
 				</div>
 			</div>
