36b3a94ae2f23cf3ede2ac9c3c445def4b2a78f9
OP <1332869575@qq.com>
2023-07-16 20:34:09 +0800

    feat(custom): 结算模块-生成订单功能

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { getCheckoutInfoAPI } from '@/apis/order'
+import { getCheckoutInfoAPI, createOrderAPI } from '@/apis/order'
+import { useRouter } from 'vue-router'
+import { useCartStore } from '@/stores/cartStore'
+const cartStore = useCartStore()
+const router = useRouter()
 const curAddress = ref({}) // 地址对象
 const checkInfo = ref({}) // 订单对象
 const getCheckInfo = async () => {
@@ -25,6 +29,31 @@ const confirm = () => {
 	curAddress.value = activeAddress.value
 	showDialog.value = false
 	activeAddress.value = {}
+}
+// 创建订单
+const createOrder = async () => {
+	const res = await createOrderAPI({
+		deliveryTimeType: 1,
+		payType: 1,
+		payChannel: 1,
+		buyerMessage: '',
+		goods: checkInfo.value.goods.map(item => {
+			return {
+				skuId: item.skuId,
+				count: item.count
+			}
+		}),
+		addressId: curAddress.value.id
+	})
+	const orderId = res.result.id // 订单id
+	router.push({
+		path: '/pay',
+		query: {
+			id: orderId
+		}
+	})
+	// 更新购物车
+	cartStore.updateCartList()
 }
 </script>
 
@@ -122,7 +151,7 @@ const confirm = () => {
 				</div>
 				<!-- 提交订单 -->
 				<div class="submit">
-					<el-button type="primary" size="large">提交订单</el-button>
+					<el-button type="primary" @click="createOrder" size="large">提交订单</el-button>
 				</div>
 			</div>
 		</div>
