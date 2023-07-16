8cdc704efcd79adf94ab449b759f3fc608c19091
OP <1332869575@qq.com>
2023-07-16 16:02:00 +0800

    feat(custom): 购物车模块-调用后端接口

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,33 +1,62 @@
 //封装购物车模块
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
+import { useUserStore } from './user'
+import { insertCartAPI, findNewCartListAPI, delCartAPI } from '@/apis/cart'
 
 export const useCartStore = defineStore(
 	'cart',
 	() => {
+		const userStore = useUserStore()
+		const isLogin = computed(() => userStore.userInfo.token)
 		// 1. 定义state - cartList
 		const cartList = ref([])
 		// 2. 定义action - addCart
-		const addCart = goods => {
-			//添加购物车操作
-			//已添加过- count+1
-			//未添加过 直接push
-			const item = cartList.value.find(item => goods.skuId === item.skuId)
-			if (item) {
-				item.count++
+		const addCart = async goods => {
+			const { skuId, count } = goods
+			if (isLogin.value) {
+				//登录之后的购物车逻辑
+				await insertCartAPI({ skuId, count })
+				updateCartList()
 			} else {
-				cartList.value.push(goods)
+				//未登录的购物车逻辑
+				//添加购物车操作
+				//已添加过- count+1
+				//未添加过 直接push
+				const item = cartList.value.find(item => goods.skuId === item.skuId)
+				if (item) {
+					item.count++
+				} else {
+					cartList.value.push(goods)
+				}
 			}
 		}
 
 		//删除购物车物品操作
-		const delCart = skuId => {
-			//方法一:找到要删除项的下标值 - splice
-			// const idx = cartList.value.findIndex(item => skuId === item.skuId)
-			// cartList.value.splice(idx, 1)
-			//方法二:使用数组的过滤方法 - filter
-			cartList.value = cartList.value.filter(item => skuId !== item.skuId)
+		const delCart = async skuId => {
+			if (isLogin.value) {
+				await delCartAPI([skuId])
+				updateCartList()
+			} else {
+				//方法一:找到要删除项的下标值 - splice
+				// const idx = cartList.value.findIndex(item => skuId === item.skuId)
+				// cartList.value.splice(idx, 1)
+				//方法二:使用数组的过滤方法 - filter
+				cartList.value = cartList.value.filter(item => skuId !== item.skuId)
+			}
 		}
+
+		//清除购物车
+		const clearCart = () => {
+			cartList.value = []
+		}
+
+		//获取最新购物车列表
+		const updateCartList = async () => {
+			const res = await findNewCartListAPI()
+			cartList.value = res.result
+		}
+
 		//计算数量和总价
 		const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
 		const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
@@ -58,7 +87,8 @@ export const useCartStore = defineStore(
 			delCart,
 			singleCheck,
 			isAll,
-			allCheck
+			allCheck,
+			clearCart
 		}
 	},
 	{
